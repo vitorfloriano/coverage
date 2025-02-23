21f66a4f54cb3bb606d4ad45a1837eb7a84ee9c0
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2025-02-22 21:17:03 -0300

    First commit

---
+++ statementcoverage.py
@@ -0,0 +1,16 @@
+# Function with multiple branches.
+import sys
+
+def StatementCoverageDemo(number):
+    if number < 0:
+        print(str(number) + ' is negative.')
+    elif number == 0:
+        print(str(number) + ' is 0.')
+    else:
+        print(str(number) + ' is positive.')
+
+# Main processing starts here.
+if __name__ == '__main__':
+    n = float(sys.argv[1])
+    StatementCoverageDemo(n)
+
